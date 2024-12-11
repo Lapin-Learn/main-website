e133b476b06fd5d87f6c3c52f9eaa13749fbdeb2
vmathu <anhthuvominh1@gmail.com>
2024-12-11 11:07:54 +0700

    feat(lap-560): add search validation and update packages

--- src/routes/_authenticated/practice/$collectionId.tsx
+++ src/routes/_authenticated/practice/$collectionId.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import CollectionDetailPage from "@/components/pages/collection-detail";
+import { searchSchema } from "@/lib/utils";
 
 export const Route = createFileRoute("/_authenticated/practice/$collectionId")({
   component: CollectionDetailPage,
+  validateSearch: searchSchema,
 });
--- src/routes/_authenticated/practice/index.tsx
+++ src/routes/_authenticated/practice/index.tsx
@@ -1,17 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { fallback, zodValidator } from "@tanstack/zod-adapter";
-import { z } from "zod";
 
 import PracticePage from "@/components/pages/practice";
-
-const searchSchema = z.object({
-  skill: fallback(
-    z.enum(["reading", "listening", "speaking", "writing"]).optional(),
-    undefined
-  ).optional(),
-});
+import { searchSchema } from "@/lib/utils";
 
 export const Route = createFileRoute("/_authenticated/practice/")({
   component: PracticePage,
-  validateSearch: zodValidator(searchSchema),
+  validateSearch: searchSchema,
 });
