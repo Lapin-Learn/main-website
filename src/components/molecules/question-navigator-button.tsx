cc1dfb82e76f571b9059f43444d9234d5ac75a9b
vmathu <91612623+vmathu@users.noreply.github.com>
2025-01-11 11:35:03 +0700

    fix(LAP-689): enhance content/result reading (#43)
    * fix(LAP-689): enhance content/result reading
    
    - make utils function for gen question id
    - enhance ui for showing answer status of multiple choices
    - fix call answer multiple choice at default cause wrong current question
    - update reset when close result dialog
    
    * style(LAP-689): change checkbox disable style
    
    (cherry picked from commit 86717dd2c4144e2898883f9d0313ae626be2e421)

--- src/components/molecules/question-navigator-button.tsx
+++ src/components/molecules/question-navigator-button.tsx
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 import useSimulatedTestStore, { useAnswerStore } from "@/hooks/zustand/use-simulated-test";
-import { cn, scrollToElementById } from "@/lib/utils";
+import { cn, genQuestionId, scrollToElementById } from "@/lib/utils";
 
 const questionNavigatorVariants = cva(
   "size-7 rounded-[4px] text-center inline-grid place-items-center text-xs duration-200 transition-colors",
@@ -13,6 +13,8 @@ const questionNavigatorVariants = cva(
         unanswered: "bg-neutral-100/50 hover:bg-neutral-100/80",
         right: "bg-green-100 text-green-900 hover:bg-green-200/50",
         wrong: "bg-red-100 text-red-900 hover:bg-red-200/50",
+        rightActive: "bg-green-500 text-white hover:bg-green-500/80",
+        wrongActive: "bg-red-500 text-white hover:bg-red-500/80",
       },
     },
   }
@@ -34,28 +36,18 @@ const QuestionNavigatorButton = ({
   const { navigateToPart } = useSimulatedTestStore();
   const { answerSheet, currentQuestion, setCurrentQuestion } = useAnswerStore();
 
+  const handleNavigation = () => {
+    navigateToPart(number, partNo);
+    scrollToElementById(genQuestionId(number));
+    setCurrentQuestion(number);
+  };
+
+  const statusVariant = determineStatus(number, currentQuestion, answerSheet, status);
+
   return (
     <button
-      onClick={() => {
-        navigateToPart(number, partNo);
-        scrollToElementById(`Question-${number}`);
-        setCurrentQuestion(number);
-      }}
-      className={cn(
-        questionNavigatorVariants({
-          status:
-            status === undefined
-              ? number.toString() == currentQuestion.toString()
-                ? "active"
-                : answerSheet[number] == null || answerSheet[number] === undefined
-                  ? "unanswered"
-                  : "answered"
-              : status
-                ? "right"
-                : "wrong",
-        }),
-        className
-      )}
+      onClick={handleNavigation}
+      className={cn(questionNavigatorVariants({ status: statusVariant }), className)}
       {...props}
     >
       {number}
@@ -64,3 +56,25 @@ const QuestionNavigatorButton = ({
 };
 
 export default QuestionNavigatorButton;
+
+function determineStatus(
+  number: number,
+  currentQuestion: number,
+  answerSheet: Record<string, string | null>,
+  status?: boolean
+) {
+  if (status !== undefined) {
+    return number === currentQuestion
+      ? status
+        ? "rightActive"
+        : "wrongActive"
+      : status
+        ? "right"
+        : "wrong";
+  }
+
+  if (number === currentQuestion) return "active";
+  if (!answerSheet[number]) return "unanswered";
+
+  return "answered";
+}
