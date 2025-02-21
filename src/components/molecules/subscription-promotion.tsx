2fdf8e1388bd2080aeab61ae65494a740c9eb842
Tran Minh Anh <tranminhanh1912@gmail.com>
2025-02-22 00:19:06 +0700

    fix(lap-789): resolve comments

--- src/components/molecules/subscription-promotion.tsx
+++ src/components/molecules/subscription-promotion.tsx
@@ -11,16 +11,16 @@ import {
 import { PulsatingButton } from "@components/ui/pulsating-button.tsx";
 import { useEvaluateSimulatedTest } from "@hooks/react-query/use-simulated-test.ts";
 import { useGetGamificationProfile } from "@hooks/react-query/useGamification.ts";
-import { CheckIcon, Loader2 } from "lucide-react";
+import { CheckIcon } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 import CarrotBasket from "@/assets/carrot-basket.svg";
+import EvaluateFail from "@/assets/evaluate_failed.svg";
 import Evaluating from "@/assets/evaluating.svg";
 import { carrotSubscription } from "@/lib/consts.ts";
 import { EnumSimulatedTestSessionStatus } from "@/lib/enums.ts";
 import { BaseSTSession } from "@/lib/types/simulated-test-session.type.ts";
 import { STCriteriaEvaluation } from "@/lib/types/simulated-test.type.ts";
-import { cn } from "@/lib/utils";
 
 import { BorderBeam } from "../magicui/border-beam";
 
@@ -31,65 +31,63 @@ type PromotionProps = {
 export function SubscriptionPromotion({ results, id, status }: PromotionProps) {
   const { data: profile, isLoading } = useGetGamificationProfile();
   const isAffordable = (profile?.carrots || 0) >= 100;
-
   const evaluateMutation = useEvaluateSimulatedTest();
-
   const { t } = useTranslation(["subscription", "shop"]);
+
+  const isNotEvaluated =
+    evaluateMutation.isIdle && status === EnumSimulatedTestSessionStatus.NOT_EVALUATED;
+  const isEvaluating =
+    evaluateMutation.isPending || status === EnumSimulatedTestSessionStatus.IN_EVALUATING;
+  const isEvaluationFailed =
+    evaluateMutation.isError || status === EnumSimulatedTestSessionStatus.EVALUATION_FAILED;
 
   return (
     !results.length &&
     !isLoading && (
-      <Card
-        className={cn(
-          "col-span-2 h-fit relative px-6 shadow-none border-[#FFCB66]",
-          evaluateMutation.isPending ? "bg-white" : "bg-gradient-to-b from-[#FCE3B4]"
-        )}
-      >
-        <CardHeader className="px-0">
-          <CardTitle className="text-primary-700">
-            {evaluateMutation.isPending ? t("evaluate.in_evaluating") : t("evaluation")}
+      <Card className="relative col-span-2 h-fit border-[#FFCB66] bg-white px-6 shadow-none">
+        <CardHeader className="mt-6 p-0">
+          <CardTitle>
+            <div
+              className={`text-heading-5 text-primary-700 ${isNotEvaluated ? "text-left" : "text-center"}`}
+            >
+              {isNotEvaluated && t("evaluation")}
+              {isEvaluating && t("evaluate.in_evaluating")}
+              {isEvaluationFailed && t("evaluate.evaluation_failed_title")}
+            </div>
           </CardTitle>
-          {!evaluateMutation.isPending && (
+          {isNotEvaluated && (
             <CardDescription>{t("description", { ns: "subscription" })}</CardDescription>
           )}
         </CardHeader>
-        <Separator />
+        {isNotEvaluated && <Separator />}
         <CardContent className="mt-6 rounded-xl px-0">
           {isAffordable ? (
-            evaluateMutation.isPending ? (
-              <div className="flex flex-col items-center justify-center gap-4">
-                <img src={Evaluating} alt="evaluating" />
-                <Typography className="text-center" variant="body2">
-                  {t("evaluate.in_evaluating_message")}
-                </Typography>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center gap-4">
-                <img src={CarrotBasket} alt="carrot-basket" />
-                <Typography variant="body2">
-                  {t(`shop.use_modal.amount`, { ns: "shop", amount: profile?.carrots, name: "" })}
-                </Typography>
+            <div className="flex flex-col items-center justify-center gap-4">
+              {isNotEvaluated && <img src={CarrotBasket} alt="carrot-basket" />}
+              {isEvaluating && <img src={Evaluating} alt="evaluating" />}
+              {isEvaluationFailed && <img src={EvaluateFail} alt="evaluate-failed" />}
+
+              <Typography className="text-center" variant="body2">
+                {isNotEvaluated &&
+                  t(`shop.use_modal.amount`, { ns: "shop", amount: profile?.carrots, name: "" })}
+                {isEvaluating && t("evaluate.in_evaluating_message")}
+                {isEvaluationFailed && t("evaluate.evaluation_failed_message")}
+              </Typography>
+
+              {!isEvaluating && (
                 <PulsatingButton
                   pulseColor="#F4926F"
                   onClick={() => evaluateMutation.mutate(id)}
-                  disabled={status == EnumSimulatedTestSessionStatus.IN_EVALUATING}
                   className="min-w-[200px]"
                 >
-                  {evaluateMutation.isPending ? (
-                    <Loader2 className="animate-spin text-white" size={24} />
-                  ) : (
-                    <div className="flex items-center gap-2">
-                      {t(`evaluate.${status}`, { ns: "subscription" })}
-                      {status === EnumSimulatedTestSessionStatus.IN_EVALUATING ? (
-                        <Loader2 className="animate-spin text-white" size={24} />
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  )}
+                  <div className="flex items-center gap-2">
+                    {evaluateMutation.isError
+                      ? t(`evaluate.evaluation_failed`, { ns: "subscription" })
+                      : t(`evaluate.${status}`, { ns: "subscription" })}
+                  </div>
                 </PulsatingButton>
-              </div>
-            )
+              )}
+            </div>
           ) : (
             <div className="flex flex-col gap-4">
               {["features.criteria", "features.price"].map((key) => (
