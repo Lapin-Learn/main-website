27b4761d5093bdbef769069121becde00a795191
MinEn1712 <91123918+MinEn1712@users.noreply.github.com>
2025-02-24 08:14:00 +0700

    feat(lap-799): blogs (#110)
    * feat(lap-799): blogs
    
    * feat(lap-799): blogs detail
    
    * feat(lap-799): integrate
    
    * chore(lap-799): rebase
    
    * fix(lap-799): resolve comments

---
+++ src/components/pages/blogs.tsx
@@ -0,0 +1,22 @@
+import { useGetBlogs } from "@/hooks/react-query/use-public";
+
+import { BlogCard } from "../organisms/blogs/blog-card";
+import { BlogsHeroBanner } from "../organisms/blogs/hero-banner";
+import LandingPageLayout from "../templates/landing-page-layout";
+
+export default function BlogsPage() {
+  const { data, isLoading } = useGetBlogs();
+
+  if (!data || isLoading) return null;
+
+  return (
+    <LandingPageLayout>
+      <BlogsHeroBanner />
+      <div className="flex flex-col gap-8 px-8 py-16 md:px-40 lg:px-80">
+        {data.items.map((blog) => (
+          <BlogCard key={blog.id} blogContent={blog} />
+        ))}
+      </div>
+    </LandingPageLayout>
+  );
+}
--- src/components/pages/landing-page.tsx
+++ src/components/pages/landing-page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useId, useState } from "react";
+import { useId } from "react";
 
 import { getAuthValueFromStorage } from "@/services";
 
@@ -8,35 +8,20 @@ import { Gamification } from "../organisms/landing-page/gamification";
 import { HeroBanner } from "../organisms/landing-page/hero-banner";
 import { MobileApp } from "../organisms/landing-page/mobile-app";
 import { SimulatedTest } from "../organisms/landing-page/simulated-test";
-import TopNavigationBar from "../organisms/landing-page/top-navigation-bar";
+import LandingPageLayout from "../templates/landing-page-layout";
 
 export default function LandingPage() {
-  const [isScrolled, setIsScrolled] = useState(false);
   const id = useId();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const heroBannerHeight = document.getElementById(id)?.offsetHeight || 0;
-      setIsScrolled(window.scrollY > heroBannerHeight);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   const isLoggedIn = getAuthValueFromStorage() !== null;
 
   return (
-    <div className="relative w-screen overflow-hidden">
-      <TopNavigationBar isScrolled={isScrolled} isLoggedIn={isLoggedIn} />
+    <LandingPageLayout>
       <HeroBanner id={id} isLoggedIn={isLoggedIn} />
       <Gamification />
       <SimulatedTest />
       <AiAssistant isLoggedIn={isLoggedIn} />
       <MobileApp />
       <Footer />
-    </div>
+    </LandingPageLayout>
   );
 }
