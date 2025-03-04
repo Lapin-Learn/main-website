b5a051c561a58ec6df20037c9f6be5f106068e2f
vmathu <anhthuvominh1@gmail.com>
2025-03-04 17:18:43 +0700

    build: remove config nginx

--- infra/main.tf
+++ infra/main.tf
@@ -6,8 +6,6 @@ resource "digitalocean_droplet" "app" {
   image  = "ubuntu-22-04-x64"
 
   ssh_keys = ["44:f0:35:66:4a:ca:bc:f5:2d:d5:90:ea:f8:6b:48:14", "b9:ae:10:6d:d6:8b:67:35:2a:39:e3:68:08:7d:bd:7b"]
-
-  user_data = file("cloud-init.yml") # Cloud-init script
 }
 
 # Output Droplet IP
