4b16123929f4653ae196901809b215ec760b301b
vmathu <anhthuvominh1@gmail.com>
2025-03-05 17:37:39 +0700

    build: enable ipv6

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,8 @@ resource "digitalocean_droplet" "app" {
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-22-04-x64"
 
+  ipv6 = true
+
   ssh_keys = ["44:f0:35:66:4a:ca:bc:f5:2d:d5:90:ea:f8:6b:48:14", "b9:ae:10:6d:d6:8b:67:35:2a:39:e3:68:08:7d:bd:7b"]
 }
 
